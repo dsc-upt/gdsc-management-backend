e241d6c9375d6be871f9d7d904d411c0c9dad834
Gaita <alex.gaita99@gmail.com>
2021-12-14 21:15:45 +0200

    Basic Entity

---
+++ GDSCManagement/GDSCManagement.API/Entities/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace GDSCManagement.API.Entities;
+
+public abstract class Entity
+{
+    public int ID { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+}
